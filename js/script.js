47ac1aead513b747af04b612b3de0632dde8e543
riccardo-giuri <richicryst@gmail.com>
2023-07-20 10:14:13 +0200

    esercizio vue hello done

---
+++ js/script.js
@@ -0,0 +1,9 @@
+Vue.createApp({
+    data() {
+        return {
+            pageTitle: "Titolo della pagina",
+            pageImage: "https://unsplash.it/300/300?image=17",
+            pageParagraph: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate nulla nesciunt doloremque facere itaque, assumenda aperiam obcaecati harum consequatur modi, voluptatum est dignissimos ad voluptatibus exercitationem nihil accusantium cum? Temporibus."
+        }
+    }
+}).mount("#app"); 
